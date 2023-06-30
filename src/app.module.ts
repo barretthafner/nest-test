ca1a31e4e8536eb473508e73dac4872c6d6ae62e
Barrett Hafner <barretth@gmail.com>
2023-06-30 13:12:58 -0700

    Remove AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DonutModule } from './donut/donut.module';
 
 @Module({
   imports: [DonutModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
